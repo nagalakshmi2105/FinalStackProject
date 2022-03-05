3d455d5f63521a62b9521af28198ff4e48ad93c4
ADMIN <ADMIN@DESKTOP-9V4PIAP>
2022-03-05 21:54:16 +0530

    Final Commit

---
+++ src/main/java/com/Smart/SmartContactApplication.java
@@ -0,0 +1,15 @@
+package com.Smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactApplication
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SmartContactApplication.class, args);
+	}
+
+}
